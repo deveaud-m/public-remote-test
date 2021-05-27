836fa8e777f176515d7a5fc2504fba0535ebc18a
Marion Deveaud <marion.deveaud@siemens.com>
2021-05-27 22:08:07 +0200

    chore(pages): clean up deploy script

--- deploy_gh_pages.sh
+++ deploy_gh_pages.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-function deploy_gh_pages {
-    mkdir -p docs
-    # Build you static site here
-    cp src/* docs/
-    git add docs/*
-    git status
-    git commit -m "docs(pages): update external static site from ${CI_JOB_ID}"
-    git log
-    git checkout -b gh-pages public/gh-pages
-    git cherry-pick -X theirs public/master
-    git push
-}
-
 if [[ ! $PUBLIC_ORIGIN ]];
 then
     echo "Environment variable PUBLIC_ORIGIN is not set"
@@ -26,9 +13,16 @@ git fetch origin
 git branch -a
 new_commit=$(git log origin/master..public/master | wc -l)
 if [ "${new_commit}" -gt 0 ]; then
-    echo "New commits where found on the remote master branch, deploy pages..."
-    deploy_gh_pages
+    echo "New commits where found on the remote master branch, deploy pages from job ${CI_JOB_ID}"
+    mkdir -p docs
+    # Build you static site here
+    cp src/* docs/
+    git add docs/*
+    git commit -m "docs(pages): update external static site from ${CI_JOB_ID}"
+    git checkout -b gh-pages public/gh-pages
+    git cherry-pick -X theirs public/master
+    git push
 else
-    echo "Nothing changed in the remote repository, nothing to do."
+    echo "Nothing changed on the remote repository, nothing to do."
     exit 0
 fi
