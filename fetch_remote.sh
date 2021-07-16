d5307ea8fcb147847f7bdef6d434f0f368f4435d
Marion Deveaud <marion.deveaud@siemens.com>
2021-07-16 23:30:58 +0200

    chore(deploy): deploy branch

--- fetch_remote.sh
+++ fetch_remote.sh
@@ -5,8 +5,8 @@ git branch -a
 new_commit=$(git log origin/main..public/main | wc -l)
 if [ "${new_commit}" -gt 0 ]; then 
     echo "New commits where found on the remote master branch, build and deploy new website"
-    git merge public/main
-    git push -u origin main
+    git switch -c public/main deploy
+    git push -u origin deploy
 else
     echo "No new commits where found on the remote master branch, exit pipeline"
 fi
